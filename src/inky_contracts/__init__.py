6fb17692323d4125dc71db874209f66e752326f5
Cyrille Hugues <cyrille.hugues@gmail.com>
2025-10-20 17:29:19 +0200

    fix(cs): ruff format + basedpyright

--- src/inky_contracts/__init__.py
+++ src/inky_contracts/__init__.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 from abc import ABC, abstractmethod
+from dataclasses import dataclass
 from enum import IntEnum
 from PIL.Image import Image
 
@@ -10,16 +11,17 @@ class Buttons(IntEnum):
     C = 16
     D = 24
 
+
+@dataclass
 class ButtonPressed:
     button: Buttons
 
+
 class ButtonListener(ABC):
     @abstractmethod
-    def listen(self) -> None:
-        ...
+    def listen(self) -> None: ...
+
 
 class Display(ABC):
     @abstractmethod
-    def show(self, image: Image) -> None:
-        ...
-
+    def show(self, image: Image) -> None: ...
